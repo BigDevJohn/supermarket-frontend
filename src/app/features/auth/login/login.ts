b4b539e64aa4196e4172aeff9da261cf0dd873c1
Death-Chronos <80175567+Death-Chronos@users.noreply.github.com>
2026-08-28 17:57:20 -0300

    feat: initial page for login

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
   selector: 'app-login',
-  styleUrl: './login.scss',
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './login.html',
+  styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+    ]),
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.loginForm.value);
+  }
+}
